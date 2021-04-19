26433c39ea34a8edb73fa5df062256f214e4cf8c
Thiago Allisson <allissonribeiro02@gmail.com>
2021-04-19 03:58:59 -0300

    Ajustando código da lista circular

--- lec.cpp
+++ lec.cpp
@@ -4,48 +4,65 @@
 
 using namespace std;
 
-typedef struct No {
-  No * proximo;
-  int valor;
+template <typename T>
+struct No {
+  No<T> * proximo;
+  T valor;
   
-  No() {
-    proximo = NULL;
-    valor = 0;
-  }
-  
-  No(int v) {
+  No(T v) {
     proximo = NULL;
     valor = v;
   }
   
   virtual ~No() {
     delete proximo;
-    proximo = NULL;
   }
-} No;
+};
 
-typedef struct ListaCircular {
-  No * cursor;
+template <typename T>
+struct ListaCircular {
+  No<T> * cursor;
   int qtdNos;
   
   ListaCircular() {
-    cursor = NULL;
-    
+    cursor = NULL;    
     qtdNos = 0;
   }
   
-  virtual ~ListaCircular() {
-    //delete cursor;
+  virtual ~ListaCircular() {    
+    cursor = NULL;
   }
   
+  bool vazio() {
+    return qtdNos == 0;
+  }
+
+  // Move o cursor adiante
   void avancar() {
     cursor = cursor->proximo;
   }
-  
-  void adicionar(int v) {
-    No * novoNo = new No(v);
+
+  string toString() {
+    if(vazio()) {
+      return "[]";
+    }
     
-    if(qtdNos == 0)  {
+    stringstream ss;
+    ss << "[" << cursor->valor;
+    No<T> * aux = cursor;
+    for(avancar(); aux != cursor; avancar()) {
+      ss << ", " << cursor->valor;
+    }
+    ss << "]";
+    
+    return ss.str();
+  }
+  
+  // Adicionar após o curso
+  void adicionar(T v) {
+    No<T> * novoNo = new No<T>(v);
+    
+    if(vazio())  {
       novoNo->proximo = novoNo;
       cursor = novoNo;
     } else {
@@ -56,23 +73,26 @@ typedef struct ListaCircular {
     qtdNos++;
   }
   
-  void remover() {
-    if(qtdNos == 0) {
-      return;
+  // Remover nó após o cursor
+  T remover() {
+    if(vazio()) {
+      throw "Lista Vazia!";
     }
-    
+
+    T temp = cursor->proximo->valor;   
     if(qtdNos == 1) {
       cursor = NULL;    
     } else {
-      No * aux = cursor->proximo;
+      No<T> * aux = cursor->proximo;
       cursor->proximo = aux->proximo;
     }
     
     qtdNos--;
+    return temp;
   }
   
   bool existe(int v) {
-    No * aux = cursor;
+    No<T> * aux = cursor;
     for(avancar(); aux != cursor; avancar()) {
       if(cursor->valor == v) {
         return true;
@@ -81,42 +101,22 @@ typedef struct ListaCircular {
     
     return false;
   }
-  
-  string toString() {
-    if(qtdNos == 0) {
-      return "{ }";
-    }
-    
-    stringstream ss;
-    ss << "{" << cursor->valor;
-    No * aux = cursor;
-    for(avancar(); aux != cursor; avancar()) {
-      ss << ", " << cursor->valor;
-    }
-    ss << "}";
-    
-    return ss.str();
-  }
-} ListaCircular;
+};
 
 int main()
 {
-  ListaCircular l;
+  ListaCircular<int> * l = new ListaCircular<int>();
   
   cout << "Adicionando 1, 2 e 4" << endl;
-  l.adicionar(1);
-  l.adicionar(2);
-  l.adicionar(4);
-  cout << l.toString() << endl;
+  l->adicionar(1);
+  l->adicionar(2);
+  l->adicionar(4);
+  cout << l->toString() << endl;
   
-  cout << "Removendo 4" << endl;
-  l.remover();
-  cout << l.toString() << endl;
+  cout << "Removendo " << l->remover() << endl;
+  cout << l->toString() << endl;
   
-  cout << "Testando o existe(2)" << endl;
-  if(l.existe(2) == true) {
-    cout << "2 cadastrado na lista" << endl;
-  }
+  delete l;
   
   return 0;
 }
